9bc515cb43942361fed897e26f0b1feddbc9341d
ThalesArede <thales_arede@hotmail.com>
2025-10-15 20:45:38 -0300

    Atualização de Desenvolvimento

---
+++ src/components/Banner.js
@@ -0,0 +1,36 @@
+import { Container, Row, Col } from "react-bootstrap";
+import headerImg from "../assets/img/header-img.svg";
+import { ArrowRightCircle } from 'react-bootstrap-icons';
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Banner = () => {
+
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="aligh-items-center">
+          <Col xs={12} md={6} xl={7}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                <span className="tagline">Bem vindo ao meu portfólio!</span>
+                <h1 className="blink-text">Olá! Eu me chamo Thales, Desenvolvedor Web e Mobile.</h1>
+                <p>Aqui apresento alguns dos meus principais projetos e competências adquiridos ao longo de 4 anos de experiência com Desenvolvimento Web, utilizando C# e .NET Core 8.</p>
+                <button onClick={() => console.log('connect')}>Vamos nos conectar? <ArrowRightCircle size={25} /></button>
+              </div>}
+            </TrackVisibility>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+                <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
+                  <img src={headerImg} alt="Header Img"/>
+                </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  )
+}
